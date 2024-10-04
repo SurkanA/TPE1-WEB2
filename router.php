ea111ba49f549a72b05c5706f3099abf58943c10
kevin artola <artolak6@gmail.com>
2024-10-04 19:21:39 -0300

    Creación de ruta de archivos

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+// leo el parametro accion
+$action = 'home'; // accion por defecto
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];  // action   => about/juan
+}
+
+// parsea la accion Ej: about/juan --> ['about', 'juan']
+$params = explode('/', $action); // genera un arreglo
+    
+switch ($params[0]) {
+    case 'home':
+       
+        break;
+    default:
+        echo "404 not found";
+        break;
+}
