568f882a779f8ab0e63f30528c98b75db807e242
Agustin Surkan <agustinsurkan@gmail.com>
2024-10-05 03:05:37 -0300

    Agregue una columna en equipo para guardar via url la imagen del equipo y actualize la primary key para que se pueda repetir el id_jugador mientras no se repita el equipo

--- futbol.sql
+++ futbol.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2024 at 12:12 AM
+-- Generation Time: Oct 05, 2024 at 08:02 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
-
-CREATE DATABASE IF NOT EXISTS futbol;
-USE futbol;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,8 +31,17 @@ CREATE TABLE `equipo` (
   `id_equipo` int(11) NOT NULL,
   `nombre_equipo` varchar(100) NOT NULL,
   `ciudad` varchar(100) NOT NULL,
-  `año_fundado` year(4) NOT NULL
+  `year_fundado` year(4) NOT NULL,
+  `imagen_url` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Dumping data for table `equipo`
+--
+
+INSERT INTO `equipo` (`id_equipo`, `nombre_equipo`, `ciudad`, `year_fundado`, `imagen_url`) VALUES
+(1, 'River', 'Buenos Aires', '1901', 'https://www.pnguniverse.com/wp-content/uploads/2020/09/Escudo-River-Plate.png'),
+(2, 'Boca', 'Buenos Aires', '1905', 'https://cdn.freebiesupply.com/logos/large/2x/boca-juniors-1-logo-png-transparent.png');
 
 -- --------------------------------------------------------
 
@@ -44,12 +50,20 @@ CREATE TABLE `equipo` (
 --
 
 CREATE TABLE `jugador` (
-  `id_jugador` int(11) NOT NULL,
+  `id_jugador` int(15) NOT NULL,
   `nombre_jugador` varchar(100) NOT NULL,
   `posicion` varchar(100) NOT NULL,
   `edad` int(11) NOT NULL,
   `id_equipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Dumping data for table `jugador`
+--
+
+INSERT INTO `jugador` (`id_jugador`, `nombre_jugador`, `posicion`, `edad`, `id_equipo`) VALUES
+(30, 'Franco Mastantuono', 'Centrocampista', 17, 1),
+(33, 'Brian Aguirre', 'Delantero', 21, 2);
 
 --
 -- Indexes for dumped tables
@@ -65,7 +79,7 @@ ALTER TABLE `equipo`
 -- Indexes for table `jugador`
 --
 ALTER TABLE `jugador`
-  ADD PRIMARY KEY (`id_jugador`),
+  ADD PRIMARY KEY (`id_jugador`,`id_equipo`),
   ADD KEY `id_equipo` (`id_equipo`);
 
 --
@@ -76,13 +90,13 @@ ALTER TABLE `jugador`
 -- AUTO_INCREMENT for table `equipo`
 --
 ALTER TABLE `equipo`
-  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jugador`
 --
 ALTER TABLE `jugador`
-  MODIFY `id_jugador` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jugador` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
