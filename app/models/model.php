7f0e642d53966aee6a2a1824b7d9b8f0118e7123
Agustin Surkan <agustinsurkan@gmail.com>
2024-10-18 03:06:15 -0300

    Addeo config.php y autodeploy en model.php

--- app/models/model.php
+++ app/models/model.php
@@ -1,12 +1,37 @@
 <?php
+require_once('config/config.php');
 class Model
 {
-    protected function crearConexion()
+    protected $db;
+
+    private function _deploy()
     {
-        $host = 'localhost';
-        $user = 'root';
-        $password = '';
-        $database = 'futbol';
+        $query = $this->db->query('SHOW TABLES');
+        $tables = $query->fetchAll();
+        if (count($tables) == 0) {
+            $sql = <<<END
+            END;
+            $this->db->query($sql);
+        }
+    }
+
+    public function ___construct()
+    {
+        $this->db = new PDO(
+            "mysql:host=" . MYSQL_HOST .
+            ";dbname=" . MYSQL_DB . ";charset=utf8",
+            MYSQL_USER,
+            MYSQL_PASS
+        );
+        $this->_deploy();
+
+    }
+    protected function createConnection()
+    {
+        $host = MYSQL_HOST;
+        $user = MYSQL_USER;
+        $password = MYSQL_PASS;
+        $database = MYSQL_DB;
 
         try {
             $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password);
