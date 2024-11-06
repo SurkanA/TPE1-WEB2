9a6cbc9e4abe0d0554cdcff1ace76558def1156e
Agustin Surkan <agustinsurkan@gmail.com>
2024-11-06 02:51:43 -0300

    Elimine una columna innecesaria en jugador

--- bbdd/futbol.sql
+++ bbdd/futbol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2024 at 03:52 AM
+-- Generation Time: Nov 06, 2024 at 06:42 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,14 +36,6 @@ CREATE TABLE `equipo` (
   `imagen_url` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
---
--- Dumping data for table `equipo`
---
-
-INSERT INTO `equipo` (`id_equipo`, `nombre_equipo`, `ciudad`, `year_fundado`, `biografia`, `imagen_url`) VALUES
-(1, 'River', 'Buenos Aires', '1901', '0', 'https://www.pnguniverse.com/wp-content/uploads/2020/09/Escudo-River-Plate.png'),
-(2, 'Boca', 'Buenos Aires', '1905', '0', 'https://cdn.freebiesupply.com/logos/large/2x/boca-juniors-1-logo-png-transparent.png');
-
 -- --------------------------------------------------------
 
 --
@@ -51,23 +43,14 @@ INSERT INTO `equipo` (`id_equipo`, `nombre_equipo`, `ciudad`, `year_fundado`, `b
 --
 
 CREATE TABLE `jugador` (
-  `id_jugador` int(11) NOT NULL,
   `nombre_jugador` varchar(50) NOT NULL,
-  `posicion` varchar(50) NOT NULL,
+  `nombre_equipo` varchar(50) NOT NULL,
+  `id_jugador` int(11) NOT NULL,
   `edad` int(11) NOT NULL,
+  `posicion` varchar(50) NOT NULL,
   `biografia` varchar(300) NOT NULL,
-  `imagen_url` varchar(300) NOT NULL,
-  `id_equipo` int(11) NOT NULL,
-  `nombre_equipo` varchar(50) NOT NULL
+  `imagen_url` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Dumping data for table `jugador`
---
-
-INSERT INTO `jugador` (`id_jugador`, `nombre_jugador`, `posicion`, `edad`, `biografia`, `imagen_url`, `id_equipo`, `nombre_equipo`) VALUES
-(6, 'Marcos Rojo', 'Defensor', 34, 'Faustino Marcos Alberto Rojo es un futbolista argentino. Juega de defensa central o lateral izquierdo y su equipo actual es el C. A. Boca Juniors de la Primera División de Argentina. Fue internacional absoluto con la selección de fútbol de Argentina.', 'https://assets.manutd.com/AssetPicker/images/0/0/12/34/795141/Marcos_Rojo_Home_00931580406064929_medium.jpg', 2, 'Boca'),
-(9, 'Miguel Borja', 'Delantero', 31, 'Miguel Ángel Borja Hernández es un futbolista colombiano. Juega de delantero en el Club Atlético River Plate de la Primera División de Argentina.​ Es internacional absoluto con la Selección de fútbol de Colombia.', 'https://www.cariverplate.com.ar/imagenes/jugadores/2024-09/1831-borja_653x667.png', 1, 'River');
 
 -- --------------------------------------------------------
 
@@ -103,8 +86,8 @@ ALTER TABLE `equipo`
 -- Indexes for table `jugador`
 --
 ALTER TABLE `jugador`
-  ADD PRIMARY KEY (`id_jugador`,`id_equipo`) USING BTREE,
-  ADD KEY `id_equipo` (`id_equipo`);
+  ADD PRIMARY KEY (`id_jugador`,`nombre_equipo`) USING BTREE,
+  ADD KEY `nombre_equipo` (`nombre_equipo`) USING BTREE;
 
 --
 -- Indexes for table `usuario`
@@ -120,29 +103,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `equipo`
 --
 ALTER TABLE `equipo`
-  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `jugador`
---
-ALTER TABLE `jugador`
-  MODIFY `id_jugador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `jugador`
---
-ALTER TABLE `jugador`
-  ADD CONSTRAINT `id_equipo` FOREIGN KEY (`id_equipo`) REFERENCES `equipo` (`id_equipo`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
